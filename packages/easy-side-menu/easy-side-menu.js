99aead68f94896dc0ccb332ae50da423bb98557c
Alberto Ramos <t00151@mail.telepac.pt>
2016-02-15 19:25:35 +0000

    RC 0.7 slide menu ok fix done

---
+++ packages/easy-side-menu/easy-side-menu.js
@@ -0,0 +1,38 @@
+//if(Meteor.isClient){
+  Template.slideMenu.rendered = function () {
+    $(document).ready(function () {
+        // make sure multiple click handlers are not registered to this
+        $('.slideout-menu-toggle').off('click');    
+        $('.slideout-menu-toggle').on('click', function(event){
+          event.preventDefault();
+          event.stopPropagation();
+
+          //console.log("SlideMenu - Click");
+
+          // create menu variables
+          var slideoutMenu = $('.slideout-menu');
+          var slideoutMenuWidth = $('.slideout-menu').width();
+          
+          // toggle open class
+          slideoutMenu.toggleClass("open");
+          
+          // slide menu Open
+          if (slideoutMenu.hasClass("open")) { 
+            slideoutMenu.animate({left: "0px"});
+   
+            // create a one-time event to close when a user clicks anywhere outside
+            $(document).one('click', function(){
+                console.log("Event on the main doc");
+              slideoutMenu.toggleClass("open");
+              slideoutMenu.animate({left: -slideoutMenuWidth}, 250); 
+            });
+          } else {
+            // slide menu close
+            slideoutMenu.animate({left: -slideoutMenuWidth}, 250);  
+          }
+        });
+    });
+  };
+
+//}
+
---
+++ packages/easy-side-menu/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: "sunkay:easy-side-menu",
+  summary: "Easily add responsive side menus to your applications",
+  version: "1.0.1",
+  git: "https://github.com/sunkay/easy-side-menu.git"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.0.1');
+  api.use(['templating'],'client');
+
+  api.addFiles('easy-side-menu.html', 'client');
+  api.addFiles(['easy-side-menu.js', 'slideout-menu.css'], 'client');
+ 
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('sunkay:easy-side-menu');
+  api.addFiles('easy-side-menu-tests.js');
+});
