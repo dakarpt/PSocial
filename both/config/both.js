b49a62a806b73fc3bacfacaa70bb747ec8ed5d15
Alberto Ramos <dakarpt@gmail.com>
2016-02-16 02:06:57 +0000

    Notifications 0.8

--- both/config/both.js
+++ both/config/both.js
@@ -32,9 +32,15 @@ TabularTables.Notifications = new Tabular.Table({
     //},
     columns: [
         {
-            data: "icon",
-            title: "Icon",
-            tmpl: Meteor.isClient && Template.showIcon
+            data: 'icon',
+            title: 'Icon',
+            createdCell: function (node, cellData, rowData) {
+                $(node).html(Blaze.toHTMLWithData(Template.showIcon, {icon: cellData}),
+                    {
+                        width: '40px',
+                        orderable: false
+                    })
+            }
         },
         {
             data: "date",
@@ -49,11 +55,22 @@ TabularTables.Notifications = new Tabular.Table({
         },
         {data: "from", title: "De:"},
         {data: "title", title: "Titulo"},
-        {data: "message", title: "Mensagem"},
-        {data: "read", title: "Leitura"},
+        //{data: "message", title: "Mensagem"},
+        //{data: "read", title: "Leitura"},
+        //{
+        //    title: "Editar",
+        //    tmpl: Meteor.isClient && Template.EditNotification
+        //},
         {
-            title: "Editar",
-            tmpl: Meteor.isClient && Template.EditNotification
+            data: '_id',
+            title: 'Editar',
+            createdCell: function (node, cellData, rowData) {
+                $(node).html(Blaze.toHTMLWithData(Template.EditNotification, {_id: cellData}),
+                    {
+                        width: '40px',
+                        orderable: false
+                    })
+            }
         },
         {
             data: '_id',
@@ -65,11 +82,12 @@ TabularTables.Notifications = new Tabular.Table({
                         orderable: false
                     })
             }
-        }],
+        }
+    ],
     extraFields: ['date', '_id'],
     order: [[ 0, 'desc' ]],
     columnDefs: [
-        { className: "col_center", targets: [ 0,3,5,6,7 ] }
+        { className: "col_center", targets: [ 0,1,3,4, 5 ] }
     ],
     stateSave: true
 });
