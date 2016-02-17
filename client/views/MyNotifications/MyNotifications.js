8e8a0bcd172c3bb2d5717a1b93953ef68038f0f4
Alberto Ramos <t00151@mail.telepac.pt>
2016-02-17 16:03:13 +0000

    alteracoes no work, notiticacoes 2

--- client/views/MyNotifications/MyNotifications.js
+++ client/views/MyNotifications/MyNotifications.js
@@ -9,21 +9,35 @@ Template.DeleteModal.events({
 });
 
 Template.MyNotifications.events({
-    'click tbody > tr > td': function (event) {
-        console.log("this", this);
-        console.log("element ", event.toElement.attributes.id);
+    'click tbody > tr': function (event) {
         var dataTable = $(event.target).closest('table').DataTable();
-        var rowData = dataTable.row(this).data();
-        var colData = dataTable.cell(this).data();
-        //var ID = Notifications.findOne({ name: rowData._id });
-        var element = event.toElement;
-        if (!event.toElement.attributes.id) {
-        //if ((element.toString().indexOf("pencil") == -1) && (element.toString().indexOf("times") == -1)) {
-            console.log("Routing to /showNotification/" + rowData._id, rowData, colData, event.toElement);
-            Router.go("/showNotification/" + rowData._id);
-        }
+        var rowData = dataTable.row(event.currentTarget).data();
+        var NOT = Notifications.findOne(rowData._id);
+
+        console.log("Rowdata:",NOT);
+        console.log("Routing to /showNotification/" + NOT._id);
+        Router.go("/showNotification/" + NOT._id);
     }
 });
+
+//Template.MyNotifications.events({
+//    'click tbody > tr > td': function (event) {
+//        console.log("this", this);
+//        console.log("element ", event.toElement.attributes.id);
+//        var dataTable = $(event.target).closest('table').DataTable();
+//        var rowData = dataTable.row(this).data();
+//        var colData = dataTable.cell(this).data();
+//        //var ID = Notifications.findOne({ name: rowData._id });
+//        var element = event.toElement;
+//        //if (!event.toElement.attributes.id) {
+//        //if ((element.toString().indexOf("pencil") == -1) && (element.toString().indexOf("times") == -1)) {
+//            console.log("Routing to /showNotification/" + rowData._id);
+//            console.log( "Row data", rowData);
+//            console.log( "to element", event.toElement);
+//            Router.go("/showNotification/" + rowData._id);
+//        //}
+//    }
+//});
 
 Template.registerHelper("iconOptions", function () {
     return [
